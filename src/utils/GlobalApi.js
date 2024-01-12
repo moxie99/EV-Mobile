dca3de24c92feba5279d8ccefb2843d08b746dd5
Adeolu David <adeolusegun99@gmail.com>
2024-01-12 23:26:30 +0100

    added the api file for fetching places

--- src/utils/GlobalApi.js
+++ src/utils/GlobalApi.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://places.googleapis.com/v1/places';
 const API_KEY = '';
 
@@ -5,6 +7,18 @@ const config = {
   headers: {
     'content-type': 'application/json',
     'X-Goog-Api-Key': API_KEY,
-    'X-Goog-FieldMask': ['places.displayName', 'places.formattedAddress'],
+    'X-Goog-FieldMask': [
+      'places.displayName',
+      'places.formattedAddress',
+      'places.location',
+      'places.evChargerOptions',
+      'places.photos',
+    ],
   },
+};
+
+const NewNearByPlace = (data) => axios.post(BASE_URL, data, config);
+
+export default {
+  NewNearByPlace,
 };
